5a45d1cbe5592e5a168080a6e2d9934af37f0a2c
Hiroka Y <yamagata.hiroka@miraitsystems.jp>
2014-11-04 16:47:35 +0900

    fixed typo.

--- test/AnalyzeTweetSpec.scala
+++ test/AnalyzeTweetSpec.scala
@@ -5,7 +5,7 @@ import org.junit.runner._
 import play.api.test._
 import play.api.test.Helpers._
 
-import contorllers.Analyze
+import controllers.Analyze
 
 @RunWith(classOf[JUnitRunner])
 class AnalyzeTweetSpec extends Specification {
@@ -13,7 +13,7 @@ class AnalyzeTweetSpec extends Specification {
   "AnalyzeTweet phase" should {
     
     "「今日は暖かい一日です」はいくつかの文節に区切られる" in {
-      Analyze.analyzeTweet("「今日は暖かい一日です」")
+      Analyze.analyzeTweet("今日は暖かい一日です")
       "" must beEqualTo ("")
     }
     
--- test/TwitterSpec.scala
+++ test/TwitterSpec.scala
@@ -5,7 +5,7 @@ import org.junit.runner._
 import play.api.test._
 import play.api.test.Helpers._
 
-import contorllers.Twitter
+import controllers.Twitter
 
 @RunWith(classOf[JUnitRunner])
 class TwitterSpec extends Specification {
