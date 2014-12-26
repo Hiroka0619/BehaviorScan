4a27c2cbeac072522ea6339966eaaf820346784d
Hiroka Y <yamagata.hiroka@miraitsystems.jp>
2014-12-26 14:49:33 +0900

    add emotion memo.

---
+++ app/models/Emotion.scala
@@ -0,0 +1,46 @@
+
+/*
+ * 
+
+<Advanced>
+Opti 希望
+Disa 失望
+Love 愛しさ
+Remo 後悔
+Subm 尊敬
+Cont 軽蔑
+Awe 畏怖
+Aggr 攻撃性
+
+<Leaf>
+Sere 平静
+Acce 容認
+Appr 心配
+Dist 混乱
+Pens 哀愁
+Bore 退屈
+Anno いらだち
+Inte 関心
+
+<Base>
+Joy 喜び
+Sadn 悲しみ
+Trus 信頼
+Disg 嫌悪
+Fear 恐怖
+Ange 怒り
+Surp 驚き
+Anti 期待
+
+<Root>
+Ecst 忘我
+Admi 敬愛
+Terr 恐怖
+Amaz 驚嘆
+Grie 悲痛
+Loat 嫌い
+Rage 憤怒
+Vigi 警戒
+
+ * 
+ */
